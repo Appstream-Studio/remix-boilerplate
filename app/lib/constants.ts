6250c5701f571bc53b6e5f5153b5cb5930fca7df
bigpun <cesar.raggio1@gmail.com>
2024-10-07 13:12:05 -0600

    [update]: add conform library to validate forms

---
+++ app/lib/constants.ts
@@ -0,0 +1,5 @@
+export const REQUIRED_ERROR = {
+  firstName: "First name is required",
+  lastName: "Last name is required",
+  role: "Role is required",
+};
--- app/routes/members._index.tsx
+++ app/routes/members._index.tsx
@@ -1,20 +1,61 @@
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
 import { ActionFunctionArgs } from "@remix-run/node";
-import { Form, redirect, useNavigate } from "@remix-run/react";
+import { Form, redirect, useActionData, useNavigate } from "@remix-run/react";
+import { z } from "zod";
+import { Field, FieldError } from "~/components/forms/field";
+import { InputConform } from "~/components/forms/input";
+import { SelectConform } from "~/components/forms/select";
 import MainLayout from "~/components/main-layout";
 import { PageHeader, PageHeaderHeading } from "~/components/page-header";
 import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 import { createNewContact } from "~/data";
+import { REQUIRED_ERROR } from "~/lib/constants";
 import url from "~/lib/url";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const submission = parseWithZod(formData, {
+    schema: z.object({
+      first: z
+        .string({ required_error: REQUIRED_ERROR.firstName })
+        .min(1)
+        .max(100),
+      last: z
+        .string({ required_error: REQUIRED_ERROR.lastName })
+        .min(1)
+        .max(100),
+      role: z
+        .enum(["dev", "pm", "apprentice", "devops", "god"], {
+          required_error: REQUIRED_ERROR.role,
+        })
+        .optional(),
+    }),
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
   await createNewContact(data);
   return redirect(url.home());
 };
 export default function MembersIndex() {
   const navigate = useNavigate();
+  const lastResult = useActionData<typeof action>();
+  const [form, fields] = useForm({
+    id: "createMember",
+    lastResult,
+    shouldValidate: "onSubmit",
+    shouldRevalidate: "onInput",
+    defaultValue: {
+      first: "",
+      last: "",
+      role: "dev",
+    },
+  });
   return (
     <MainLayout
       title="Members"
@@ -25,22 +66,55 @@ export default function MembersIndex() {
         </Button>
       }
     >
-      <Form method="post">
+      <Form method="post" id={form.id} onSubmit={form.onSubmit}>
         <div className="flex flex-col gap-6 items-center">
           <div className="max-w-2xl w-full">
             <PageHeader className="gap-4">
               <PageHeaderHeading>
                 Here you can create a new member
               </PageHeaderHeading>
-              <Input
-                type="text"
-                id="first"
-                name="first"
-                placeholder="FirstName"
-              />
-              <Input type="text" id="last" name="last" placeholder="LastName" />
+
+              <Field>
+                <Label htmlFor={fields.first.id}>First name</Label>
+                <InputConform
+                  meta={fields.first}
+                  type="text"
+                  placeholder="First name..."
+                />
+                {fields.first.errors && (
+                  <FieldError>{fields.first.errors}</FieldError>
+                )}
+              </Field>
+
+              <Field>
+                <Label htmlFor={fields.last.id}>Last name</Label>
+                <InputConform
+                  meta={fields.last}
+                  type="text"
+                  placeholder="Last name..."
+                />
+                {fields.last.errors && (
+                  <FieldError>{fields.last.errors}</FieldError>
+                )}
+              </Field>
               {/* We need an avatar input that receives a URL :) */}
-              <Input type="text" id="role" name="role" placeholder="Role" />
+              <Field>
+                <Label htmlFor={fields.role.id}>Role</Label>
+                <SelectConform
+                  placeholder="Select a Role"
+                  meta={fields.role}
+                  items={[
+                    { value: "dev", name: "Developer" },
+                    { value: "pm", name: "Project manager" },
+                    { value: "apprentice", name: "Apprentice" },
+                    { value: "devops", name: "DevOps" },
+                    { value: "god", name: "GOD (does everything)" },
+                  ]}
+                />
+                {fields.role.errors && (
+                  <FieldError>{fields.role.errors}</FieldError>
+                )}
+              </Field>
               <Button className="w-full" type="submit">
                 Save
               </Button>
